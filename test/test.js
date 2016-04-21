0d2fb7adaafc5966778aa952f201d2857d127add
rohithzr@live.com <rohithzr@live.com>
2016-04-21 11:47:51 +0530

    Added All Mocha Tests

--- test/test.js
+++ test/test.js
@@ -1,31 +1,79 @@
 var WWO = require('../lib/index');
 
 var client = WWO.createClient({
-  // key: process.env.key,
-  key: '9d378416be224717b92172807162004',
-  responseType: 'json'
+    key: '9d378416be224717b92172807162004',
+    responseType: 'json',
+    subscription: 'premium',
+    locale: 'EN'
 });
 
-describe('API requests',function(){
-  describe('searchApi',function(){
-    it('response should not return an error', function(done){
-      client.searchApi({
-        q : "Delhi"
-      }, function(err, result){
-          if(err) throw err;
-          done();
-      });
+describe('API requests', function() {
+    describe('searchApi', function() {
+        it('response should not return an error', function(done) {
+            client.searchApi({
+                q: "Delhi"
+            }, function(err, result) {
+                if (err) throw err;
+                done();
+            });
+        });
     });
-  });
-  describe('localWeatherApi',function(){
-    it('response should not return an error', function(done){
-      client.localWeatherApi({
-        q : "London",
-        num_of_days : "5"
-      }, function(err, result){
-          if(err) throw err;
-          done();
-      });
+    describe('localWeatherApi', function() {
+        it('response should not return an error', function(done) {
+            this.timeout(30000);
+            client.localWeatherApi({
+                q: "London",
+                num_of_days: "1"
+            }, function(err, result) {
+                if (err) throw err;
+                done();
+            });
+        });
     });
-  });  
+    describe('timeZoneApi', function() {
+        it('response should not return an error', function(done) {
+            this.timeout(30000);
+            client.timeZoneApi({
+                q: "208021"
+            }, function(err, result) {
+                if (err) throw err;
+                done();
+            });
+        });
+    });
+    describe('skiWeatherApi', function() {
+        it('response should not return an error', function(done) {
+            this.timeout(30000);
+            client.skiWeatherApi({
+                q: "London",
+                num_of_days: "2"
+            }, function(err, result) {
+                if (err) throw err;
+                done();
+            });
+        });
+    });
+    describe('marineWeatherApi', function() {
+        it('response should not return an error', function(done) {
+            this.timeout(30000);
+            client.marineWeatherApi({
+                q: "48.834,2.394"
+            }, function(err, result) {
+                if (err) throw err;
+                done();
+            });
+        });
+    });
+    describe('historicalWeatherApi', function() {
+        it('response should not return an error', function(done) {
+            this.timeout(30000);
+            client.historicalWeatherApi({
+                q: "London",
+                date: "1995-05-02"
+            }, function(err, result) {
+                if (err) throw err;
+                done();
+            });
+        });
+    });
 });
